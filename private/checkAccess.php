96b777351e1cad9b8f785a3d9e30c78541214487
Andrew Rivett <veggiefrog@gmail.com>
2012-06-26 01:24:29 +0000

    Fixed permissions with users uploading avatars

--- private/checkAccess.php
+++ private/checkAccess.php
@@ -26,24 +26,29 @@ function ciniki_images_checkAccess($ciniki, $business_id, $method) {
 	}
 
 	//
-	// Users who are an owner or employee of a business can see the business atdo
+	// Users who are an owner or employee of a business can see the business images
 	//
-	$strsql = "SELECT business_id, user_id FROM ciniki_business_users "
-		. "WHERE business_id = '" . ciniki_core_dbQuote($ciniki, $business_id) . "' "
-		. "AND user_id = '" . ciniki_core_dbQuote($ciniki, $ciniki['session']['user']['id']) . "' "
-		. "AND package = 'ciniki' "
-		. "AND (permission_group = 'owners' OR permission_group = 'employees') "
-		. "";
-	require_once($ciniki['config']['core']['modules_dir'] . '/core/private/dbHashQuery.php');
-	$rc = ciniki_core_dbHashQuery($ciniki, $strsql, 'businesses', 'user');
-	if( $rc['stat'] != 'ok' ) {
-		return $rc;
-	}
-	//
-	// If the user has permission, return ok
-	//
-	if( isset($rc['rows']) && isset($rc['rows'][0]) 
-		&& $rc['rows'][0]['user_id'] > 0 && $rc['rows'][0]['user_id'] == $ciniki['session']['user']['id'] ) {
+	if( $business_id > 0 ) {
+		$strsql = "SELECT business_id, user_id FROM ciniki_business_users "
+			. "WHERE business_id = '" . ciniki_core_dbQuote($ciniki, $business_id) . "' "
+			. "AND user_id = '" . ciniki_core_dbQuote($ciniki, $ciniki['session']['user']['id']) . "' "
+			. "AND package = 'ciniki' "
+			. "AND (permission_group = 'owners' OR permission_group = 'employees') "
+			. "";
+		require_once($ciniki['config']['core']['modules_dir'] . '/core/private/dbHashQuery.php');
+		$rc = ciniki_core_dbHashQuery($ciniki, $strsql, 'businesses', 'user');
+		if( $rc['stat'] != 'ok' ) {
+			return $rc;
+		}
+		
+		//
+		// If the user has permission, return ok
+		//
+		if( isset($rc['rows']) && isset($rc['rows'][0]) 
+			&& $rc['rows'][0]['user_id'] > 0 && $rc['rows'][0]['user_id'] == $ciniki['session']['user']['id'] ) {
+			return array('stat'=>'ok');
+		}
+	} else {
 		return array('stat'=>'ok');
 	}
 
